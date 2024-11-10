6bde5da92c7135bd5b8d65f0adc77f820e651d95
Ali Sina <ali.sina1447@gmail.com>
2024-11-10 07:46:08 -0500

    initial commit

---
+++ src/test/java/ca/alisina/ForgetLens/ForgetLensApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.alisina.ForgetLens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForgetLensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
